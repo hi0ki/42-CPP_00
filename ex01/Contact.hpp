48bd50f99c7c48335ff733c30657cf6de73a1432
hi0ki <ansarimehdi518@gmail.com>
2025-01-07 13:37:45 +0100

    ex01

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -11,32 +11,17 @@
 			std::string nickname;
 			std::string darkest_secreret;
 		public:
-			void set_number(std::string phonenumber)
-			{
-				number = phonenumber;
-			}
-			void set_fname(std::string fname)
-			{
-				first_name = fname;
-			}
-			void set_lname(std::string lname)
-			{
-				last_name = lname;
-			}
-			void set_nickname(std::string nick)
-			{
-				nickname = nick;
-			}
-			void set_secreret(std::string d_secreret)
-			{
-				darkest_secreret = d_secreret;
-			}
+			void set_number(std::string phonenumber);
+			void set_fname(std::string fname);
+			void set_lname(std::string lname);
+			void set_nickname(std::string nick);
+			void set_secreret(std::string d_secreret);
 
-			std::string  get_number() {return (number);};
-			std::string get_fname() {return (first_name);};
-			std::string get_lname() {return (last_name);};
-			std::string get_secreret() {return (darkest_secreret);};
-			std::string get_nickname() {return (nickname);};
+			std::string get_number();
+			std::string get_fname();
+			std::string get_lname();
+			std::string get_secreret();
+			std::string get_nickname();
 	};
 
 #endif
---
+++ ex02/Account.cpp
@@ -0,0 +1,12 @@
+#include "Account.hpp"
+	Account acc;
+
+
+		int	Account::getNbAccounts( void )
+		{
+			retunr _nbAccounts;
+		}
+int main{
+	atexit(ll);
+	return ;
+}
